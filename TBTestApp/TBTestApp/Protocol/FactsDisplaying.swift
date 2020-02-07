d70471cc4986e2881a5e4737d34f17276de0c8b9
Tushar Bole <tusharbole@Tushars-MacBook-Pro.local>
2020-02-07 14:34:55 +1100

    Added Display and Presenting protocol for Presenter and ViewController

---
+++ TBTestApp/TBTestApp/Protocol/FactsDisplaying.swift
@@ -0,0 +1,18 @@
+//
+//  FactsDisplaying.swift
+//  TBTestApp
+//
+//  Created by Tushar Bole on 7/2/20.
+//  Copyright © 2020 Tushar Bole. All rights reserved.
+//
+
+protocol FactsDisplaying {
+    
+    /// Ask display to set the display data
+    /// - Parameter data: data of type FactsData received from API
+    func setDisplayData(_ data: FactsData)
+    
+    /// Ask display to show error message
+    /// - Parameter message: error message to be displayed
+    func showErrorMessage(_ message: String)
+}
---
+++ TBTestApp/TBTestApp/Protocol/FactsPresenting.swift
@@ -0,0 +1,13 @@
+//
+//  FactsPresenting.swift
+//  TBTestApp
+//
+//  Created by Tushar Bole on 7/2/20.
+//  Copyright © 2020 Tushar Bole. All rights reserved.
+//
+
+protocol FactsPresenting {
+    
+    /// Tell presenter that view is visible and ready to perform next action
+    func viewDidBecomeVisible()
+}
