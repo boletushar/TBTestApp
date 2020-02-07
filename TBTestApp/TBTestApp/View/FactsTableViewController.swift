fd1897b96129bebe45e3d573d45a5ea06f112a5d
Tushar Bole <tusharbole@Tushars-MacBook-Pro.local>
2020-02-07 14:20:02 +1100

    Added FactsTableView controller

---
+++ TBTestApp/TBTestApp/View/FactsTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FactsTableViewController.swift
+//  TBTestApp
+//
+//  Created by Tushar Bole on 7/2/20.
+//  Copyright © 2020 Tushar Bole. All rights reserved.
+//
+
+import UIKit
+
+class FactsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+}
