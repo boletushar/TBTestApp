ccf66c0963f72d375e7a667ddc27aa28156c1413
Tushar Bole <tusharbole@Tushars-MacBook-Pro.local>
2020-02-07 14:26:40 +1100

    Added model property docs

--- TBTestApp/TBTestApp/Model/FactsData.swift
+++ TBTestApp/TBTestApp/Model/FactsData.swift
@@ -10,6 +10,9 @@ import Foundation
 
 struct FactsData: Codable {
     
+    /// String title used to show screen title
     let title: String
+    
+    /// Array of Fact which will be listed on screen
     let rows: [Fact]
 }
