454b0ca3ed29cf7c27a0238f456f51ec151f3d35
Tushar Bole <tusharbole@Tushars-MacBook-Pro.local>
2020-02-07 14:49:32 +1100

    Added custom tableView cell to show Fact data

---
+++ TBTestApp/TBTestApp/View/FactTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FactTableViewCell.swift
+//  TBTestApp
+//
+//  Created by Tushar Bole on 7/2/20.
+//  Copyright © 2020 Tushar Bole. All rights reserved.
+//
+
+import UIKit
+
+class FactTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+}
