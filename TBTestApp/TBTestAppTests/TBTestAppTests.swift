a1f8329c6866a59041b85733ba104aa956580668
Tushar Bole <boletushar@gmail.com>
2020-02-07 20:37:05 +1100

    Added unit test for Dependency injection

--- TBTestApp/TBTestAppTests/TBTestAppTests.swift
+++ TBTestApp/TBTestAppTests/TBTestAppTests.swift
@@ -7,27 +7,57 @@
 //
 
 import XCTest
+import Swinject
+@testable import TBTestApp
 
 class TBTestAppTests: XCTestCase {
+    
+    var presenter: FactsPresenting?
+    
+    let container = Container()
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // Put setup code here. This method is called before the
+        // invocation of each test method in the class.
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // Put teardown code here. This method is called after the
+        // invocation of each test method in the class.
+        presenter = nil
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    
+    func mockRegistration() {
+        
+        container.register(FactsProviding.self) { (resolver) -> FactsProviderService in
+            FactsProviderService()
         }
+        
+        container.register(FactsPresenting.self) { (resolver, display) -> FactsPresenter in
+            FactsPresenter(
+                display: display,
+                factsProvider: resolver.resolve(FactsProviding.self)!)
+        }.inObjectScope(.weak)
+        
+        container.register(FactsDisplaying.self) { (resolver) -> FactsDisplaying in
+            let viewController = FactsTableViewController()
+            viewController.presenter = resolver.resolve(
+                FactsPresenting.self,
+                argument: viewController as FactsDisplaying)
+            return viewController
+        }.inObjectScope(.container)
     }
 
+    func testDependencyInjection() {
+        
+        mockRegistration()
+        
+        let viewController = container.resolve(FactsDisplaying.self) as? FactsTableViewController
+        XCTAssertTrue(viewController != nil, "Viewcontroller Dependency injection failed")
+        XCTAssertTrue(viewController?.presenter != nil,
+                      "Viewcontroller unable to initiate presenter")
+        
+        let factsProvider = container.resolve(FactsProviding.self)
+        XCTAssertTrue(factsProvider != nil, "")
+    }
 }
