65b3fb3891cc4f4e571d95c764efb938a5f6ff2d
Tushar Bole <tusharbole@Tushars-MacBook-Pro.local>
2020-02-07 14:50:13 +1100

    Added required initializer

--- TBTestApp/TBTestApp/View/FactTableViewCell.swift
+++ TBTestApp/TBTestApp/View/FactTableViewCell.swift
@@ -13,4 +13,8 @@ class FactTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
